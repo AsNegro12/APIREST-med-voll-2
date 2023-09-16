b61d6c3ad9da98baa4d5e867bd53396cf5b06cf7
UrielLaptop <floresruizur13l@outlook.com>
2023-09-16 00:35:06 -0700

    Se ledio un limite de tamaño a rfc

--- src/main/java/med/voll/api/domain/medico/DatosRegistroMedico.java
+++ src/main/java/med/voll/api/domain/medico/DatosRegistroMedico.java
@@ -1,10 +1,7 @@
 package med.voll.api.domain.medico;
 
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.Email;
-import jakarta.validation.constraints.NotBlank;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.Pattern;
+import jakarta.validation.constraints.*;
 import med.voll.api.domain.direccion.DatosDireccion;
 
 public record DatosRegistroMedico(
@@ -17,7 +14,7 @@ public record DatosRegistroMedico(
         @NotBlank
         String telefono,
         @NotBlank
-
+        @Size(min = 10,max = 10)
         String rfc,
         @NotNull
         Especialidad especialidad,
